8022739db7741186d796ee083e1d5d4e49f597a4
Alif Panglima Nurda <alifnurda@gmail.com>
2023-12-08 19:07:04 +0700

    added db configuration

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// DB Configuration
+mongoose
+  .connect(process.env.URI_DB)
+  .then(function () {
+    console.log('DB Connected')
+  })
+  .catch(function (err) {
+    if (err) console.error(err)
+  })
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
